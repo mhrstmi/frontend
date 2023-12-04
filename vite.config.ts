dd073996f528f18e87b4dd4446e88f7100f82439
Mohammad Hussein Rostami <mohammad.hussein.rostami@gmail.com>
2023-12-04 21:12:55 +0330

    fix: auto deploy

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       'xmlhttprequest-ssl': './node_modules/engine.io-client/lib/xmlhttprequest.js',
     },
   },
-  base: "/frontend",
+  base: "/frontend/",
   plugins: [
     react(), 
     tsconfigPaths(),
