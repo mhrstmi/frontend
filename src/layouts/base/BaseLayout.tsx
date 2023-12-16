eea1fc57f9f6886076a198b06de664db5f71a66d
Mohammad Hussein Rostami <mohammad.hussein.rostami@gmail.com>
2023-12-17 01:50:15 +0330

    feat: add home page structure

--- src/layouts/base/BaseLayout.tsx
+++ src/layouts/base/BaseLayout.tsx
@@ -1,9 +1,42 @@
+import { Button, Layout } from 'antd'
+import { Footer, Header } from 'antd/es/layout/layout'
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { Text } from '../../components'
+import Inog from '@assets/images/in-the-name-of-god.png'
+import { PhoneOutlined } from '@ant-design/icons'
+import urls from '../../routes/urls'
 
 const BaseLayout = () => {
+  const navigate = useNavigate()
+  
   return (
-    <div><Outlet /></div>
+    <Layout className='overflow-x-hidden'>
+      <Header className=' bg-light-green flex items-center justify-center p-3 h-20'>
+        <div className='w-2/3 grid grid-cols-3'>
+          <div className='flex items-center justify-start'>
+            <Button className='bg-mid-green w-fit' onClick={() => navigate(urls.login)}>
+              <Text fontSize='base' fontWeight='normal' className='text-white'>ورود</Text>
+            </Button>
+          </div>
+          <div className='flex justify-center items-center rounded-lg'>
+            <div className='w-fit max-h-[70px] rounded-r-md rounded-se-[100px] bg-black flex justify-center items-center bg-opacity-30'>
+              <img className='w-2/3 object-fill' src={Inog} /> 
+            </div>  
+          </div>
+          <div dir="ltr" className='flex items-center gap-3' >
+            <PhoneOutlined className='mb-2 text-lg text-blue-500' />
+            <Text fontSize='sm' fontWeight='heavy' className='text-dark-green flex items-center'>+98 903-786-543</Text>
+          </div>
+        </div>
+      </Header>
+      <Layout>
+        <Outlet />
+      </Layout>
+      {/* <Footer>
+        hi
+      </Footer> */}
+    </Layout>
   )
 }
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,59 @@
-import React from 'react'
 import urls from '../../routes/urls'
-import { Link, useNavigate } from 'react-router-dom'
-import { Button } from 'antd'
+import { useNavigate } from 'react-router-dom'
+import { Button, Radio, Tabs } from 'antd'
+import Agha from '@assets/images/agha.png'
+import React, { useState } from 'react';
+import type { RadioChangeEvent, TabsProps } from 'antd';
+import type { SizeType } from 'antd/es/config-provider/SizeContext';
+import { Text } from '../../components';
+
+const tabs: TabsProps['items'] = [
+  {
+    label: <Text fontSize='base' fontWeight='heavy' className='text-dark-green'>پرسشنامه ها</Text>,
+    key: '1',
+    children: <div className='m-5 p-5 bg-white rounded-lg flex items-center justify-center h-full'>تب اول</div>,
+  },
+  {
+    label: <Text fontSize='base' fontWeight='heavy' className='text-dark-green'>دانشنامه ها</Text>,
+    key: '2',
+    children: <div className='m-5 p-5 bg-white rounded-lg flex items-center justify-center h-full'>تب دوم</div>,
+  },
+  {
+    label: <Text fontSize='base' fontWeight='heavy' className='text-dark-green'>کتابخانه</Text>,
+    key: '3',
+    children: <div className='m-5 p-5 bg-white rounded-lg flex items-center justify-center h-full'>تب سوم</div>,
+  },
+  {
+    label: <Text fontSize='base' fontWeight='heavy' className='text-dark-green'>تقویم مقاومت</Text>,
+    key: '4',
+    children: <div className='m-5 p-5 bg-white rounded-lg flex items-center justify-center h-full'>تب چهارم</div>,
+  },
+  {
+    label: <Text fontSize='base' fontWeight='heavy' className='text-dark-green'>تقویم مدرسه</Text>,
+    key: '5',
+    children: <div className='m-5 p-5 bg-white rounded-lg flex items-center justify-center h-full'>تب پنجم</div>,
+  },
+]
 
 const Home = () => {
   const navigate = useNavigate()
+
   return (
-    <div className='h-screen w-screen flex justify-center items-center gap-5'>
-      <Button className='bg-blue-500 text-white' onClick={() => navigate(urls.login)}>صفحه ورود به داشبورد</Button>
-      <Button className='bg-red-500 text-white' onClick={() => navigate(urls.adminDashboard)}>صفحه داشبورد ادمین</Button>
+    <div className='h-screen w-screen gap-5 bg-light-green'>
+      <div className='flex items-center justify-center p-5'>
+        <img className='w-2/3 rounded-md  object-cover' src={Agha} />
+      </div>
+      <div className='w-full flex justify-center items-center'>
+        <div className='w-2/3'>
+          <Tabs
+            defaultActiveKey="1"
+            centered
+            type="card"
+            size={'large'}
+            items={tabs}
+          />
+        </div>
+      </div>
     </div>
   )
 }
